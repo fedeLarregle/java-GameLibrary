3e3f6a924e78b84a0e8d687729d927ce0594954c
federico <fede.larregle@gmail.com>
2017-05-03 20:23:09 -0300

    Added a really basic enemy player collision

--- src/com/puebla/TiledGame/model/Enemy.java
+++ src/com/puebla/TiledGame/model/Enemy.java
@@ -1,5 +1,6 @@
 package com.puebla.TiledGame.model;
 
+import com.puebla.TiledGame.gameStates.GameOverState;
 import com.puebla.TiledGame.main.Game;
 import com.puebla.TiledGame.tileMap.TileMap;
 
@@ -7,7 +8,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 /**
- * Created by federico on 29/04/17.
+ * @author federico on 29/04/17.
+ * @email fede.larregle@gmail.com
  */
 public class Enemy implements Entity{
 
@@ -33,7 +35,7 @@ public class Enemy implements Entity{
         this.playerToSeek = playerToSeek;
         this.width = 24;
         this.height = 24;
-        this.moveSpeed = 0.2;
+        this.moveSpeed = 0.15;
         this.maxSpeed = 2.5;
         this.stoppingSpeed = 0.4;
     }
@@ -106,6 +108,12 @@ public class Enemy implements Entity{
 
     @Override
     public void update() {
+
+        /* Checking for player enemy collision */
+        if (  ((this.getX() - playerToSeek.getX()) >= 0 && ((this.getX() - playerToSeek.getX()) < width)) &&
+        ((this.getY() - playerToSeek.getY()) >= 0 && ((this.getY() - playerToSeek.getY()) < height)) ) {
+            game.setGameState(new GameOverState(game));
+        }
 
         if ( ((this.playerToSeek.getX() - this.getX()) > 0) ) {
             this.moveRight();
