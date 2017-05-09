0f268c242af6738006bef295d6586379b1716816
federico <fede.larregle@gmail.com>
2017-05-09 20:48:54 -0300

    Made Diamond class Collidable and WIDTH and HEIGHT static

--- src/com/puebla/TiledGame/model/Diamond.java
+++ src/com/puebla/TiledGame/model/Diamond.java
@@ -1,6 +1,7 @@
 package com.puebla.TiledGame.model;
 
 import com.puebla.TiledGame.main.Game;
+import com.puebla.TiledGame.manager.Collidable;
 import com.puebla.TiledGame.tileMap.TileMap;
 
 import java.awt.Color;
@@ -10,21 +11,26 @@ import java.awt.Graphics;
  * @author federico on 06/05/17.
  * @email fede.larregle@gmail.com
  */
-public class Diamond implements Entity{
-
-    private double x;
-    private double y;
-    private final double width;
-    private final double height;
+public class Diamond implements Entity, Collidable{
 
     private Game game;
     private TileMap tileMap;
 
+    private double x;
+    private double y;
+
+    private final static double WIDTH;
+    private final static double HEIGHT;
+
+
+    static {
+        WIDTH = 32;
+        HEIGHT = 32;
+    }
+
     public Diamond(Game game, TileMap tileMap) {
         this.game = game;
         this.tileMap = tileMap;
-        this.width = 32;
-        this.height = 32;
     }
 
 
@@ -37,6 +43,26 @@ public class Diamond implements Entity{
     public void draw(Graphics graphics) {
 
         graphics.setColor(Color.WHITE);
-        graphics.fillOval(((int)x), ((int)y), ((int)width), ((int)height));
+        graphics.fillOval(((int)x), ((int)y), ((int)WIDTH), ((int)HEIGHT));
+    }
+
+    @Override
+    public int getX() {
+        return ((int)this.x);
+    }
+
+    @Override
+    public int getY() {
+        return ((int)this.y);
+    }
+
+    @Override
+    public int getWidth() {
+        return ((int)WIDTH);
+    }
+
+    @Override
+    public int getHeight() {
+        return ((int)HEIGHT);
     }
 }
